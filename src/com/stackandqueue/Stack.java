66f29ab1575962a3b98c80fd967148c449d76fac
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-14 18:37:25 +0530

    ADDED: added comments and annotations to methods of stack

--- src/com/stackandqueue/Stack.java
+++ src/com/stackandqueue/Stack.java
@@ -5,15 +5,26 @@ public class Stack <Type extends Comparable<Type>> extends LinkedList<Type>{
 		super();
 	}
 
+	/**
+	 * add item to the head of the stack
+	 * @param item
+	 */
 	public void push(Type item) {
 		super.add(item);
 	}
 	
+	/**
+	 * remove item at the head of stack then remove element from head and shift head to the next element
+	 */
 	@Override
 	public Type pop() {
 		return super.pop();
 	}
 	
+	/**
+	 * returns the element at head position of the stack and do nothing operations
+	 * @return head item from linked list
+	 */
 	public Type Peek() {
 		return super.headItem();
 	}
